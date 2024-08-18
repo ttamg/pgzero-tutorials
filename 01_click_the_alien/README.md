c9bdd3bb0a56da7ec8fc596297945c43fbd970f2
Matt G <mdgosden@gmail.com>
2024-08-18 20:22:56 +0000

    Alien example

---
+++ 01_click_the_alien/README.md
@@ -0,0 +1,8 @@
+# Click the Alien
+
+A simple game using the mouse to click on the alien object and see how it impacts the actor / sprite.
+
+Inspired by good article - https://www.mattlayman.com/blog/2019/teach-kid-code-pygame-zero/
+
+Sprites from Kenney.nl - https://kenney.nl/assets/platformer-art-deluxe
+
--- 01_click_the_alien/main.py
+++ 01_click_the_alien/main.py
@@ -1,1 +1,54 @@
-print("Hello World")
+import random
+
+bobby = Actor('alien_swim_1.png')
+bobby.topright = 0, 10
+bobby.swim_phase = 0
+bobby.hurt = False
+
+WIDTH = 712
+HEIGHT = 508
+
+
+def draw():
+  screen.fill((40, 6, 53))
+  bobby.draw()
+
+
+def update():
+  # Move to the right
+  bobby.left += 1
+
+  # Scroll around screen
+  if bobby.left > WIDTH:
+    bobby.right = 0
+    bobby.top = random.randint(0, 300)
+
+  # Swim
+  if not bobby.hurt:
+    bobby.swim_phase += 1
+    if bobby.swim_phase < 10:
+      bobby.image = 'alien_swim_1.png'
+    else:
+      bobby.image = 'alien_swim_2.png'
+      if bobby.swim_phase > 25:
+        bobby.swim_phase = 0
+
+
+def on_mouse_down(pos):
+  if bobby.collidepoint(pos):
+    print("Good shot!")
+    set_alien_hurt()
+  else:
+    print("You missed me!")
+
+
+def set_alien_hurt():
+  print("Eek!")
+  # sounds.ouch.play()
+  bobby.hurt = True
+  bobby.image = 'alien_hurt'
+  clock.schedule_unique(set_alien_normal, 1.0)
+
+
+def set_alien_normal():
+  bobby.hurt = False
